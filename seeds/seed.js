0d1a332fb107d6d92500b1a2f7df2f1c51a1e1ff
Philip Browning <philip741@gmail.com>
2021-10-26 17:41:23 -0400

    Added seed data and updated seeds code to use them

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,11 @@
 const sequelize = require('../config/connection');
 const Employee = require('../models/employee');
+const Role = require('../models/role');
+const Department = require('../models/department');
 
 const emplSeedData = require('./employeeSeedData.json');
+const deptSeedData = require('./deptSeedData.json');
+const roleSeedData = require('./roleSeedData.json');
 
 const seedDatabase = () => {
     return sequelize.sync({
@@ -9,6 +13,8 @@ const seedDatabase = () => {
     })
     .then(() => {
       Employee.bulkCreate(emplSeedData)
+      Role.bulkCreate(roleSeedData)
+      Department.bulkCreate(deptSeedData)
         .then(() => {
           console.log('All Seeds Planted');
         });
