d519aa0441e10e42144b33289bf1b5463a4b4d45
Philip Browning <philip741@gmail.com>
2021-10-27 12:33:04 -0400

    Added code to view all departments

--- libs/dbFuncs.js
+++ libs/dbFuncs.js
@@ -1,12 +1,14 @@
-const path = require('path');
-const  Employee = require(path.join(__dirname, '../models/employee'));
-const sequelize = require('../config/connection');
-const cTable = require('console.table');
+const path = require("path");
+const  Employee = require(path.join(__dirname, "../models/employee"));
+const  Department = require(path.join(__dirname, "../models/department"));
+const  Roles = require(path.join(__dirname, "../models/roles"));
+const sequelize = require("../config/connection");
+const cTable = require("console.table");
 
 
 async function viewAllEmpl() {
  const employees = await Employee.findAll({
-     attributes: ['id','first_name','last_name'],
+     attributes: ["id","first_name","last_name"],
      raw: true
  })
  console.table(employees);
@@ -17,7 +19,16 @@ function viewAllRoles() {
 }
 
 function viewAllDept () {
+ const departments = await Department.findAll({
+     attributes: ["id","name"],
+     raw: true
+ })
+ console.table(departments);
        
 }
 
-module.exports = {viewAllEmpl};
+function addDepartment () {
+
+}
+
+module.exports = {viewAllEmpl, viewAllRoles, viewAllDept};
